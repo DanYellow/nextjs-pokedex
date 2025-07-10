298be466a6b177716a1163863333ce27acd08bbf
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-07-10 19:08:42 +0200

    Implement API

---
+++ src/app/_api/pokeapi.ts
@@ -0,0 +1,36 @@
+export const fetchPokemonDetails = async (pkmnId: number) => {
+    try {
+        const req = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkmnId}`);
+        return req.json();
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+export const fetchPokemonExternalData = async (pkmnId: number) => {
+    try {
+        const req = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pkmnId}`);
+        return req.json();
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+export const fetchEvolutionChain = async (url: string) => {
+    try {
+        const req = await fetch(url);
+        return req.json();
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+export const fetchAbilityData = async (url: string) => {
+    try {
+        const req = await fetch(url);
+        return req.json();
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
