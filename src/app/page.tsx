894607beff06562bebd72152e9a14f6e500bf57f
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-07-11 08:32:19 +0200

    Catch error when fetch Pokedex fails

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,26 @@
 import { fetchPokemonForGeneration } from "@/app/_api/tyradex";
 import PokedexEntry from "@/app/_components/PokedexEntry";
 
-export default async function Home() {
-    const data = await fetchPokemonForGeneration();
+import type { IPokemon } from "@/app/_types/Pokemon";
 
-  return (
-    <div>
-      <main>
-        <ol className="pokedex grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-4 mb-4 mx-auto max-w-6xl mt-2">
-          {data.slice(0, 5).map((pokemon: any) => (
-            <PokedexEntry
-                key={pokemon.pokedex_id}
-                id={pokemon.pokedex_id}
-                name={pokemon.name.fr}
-                sprite={pokemon.sprites.regular}
-                listTypes={pokemon.types}
-            />
-          ))}
-        </ol>
-      </main>
-    </div>
-  );
+export default async function Home() {
+    const { data, hasReachedEnd } = await fetchPokemonForGeneration();
+
+    return (
+        <div>
+            <main>
+                <ol className="pokedex grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-4 mb-4 mx-auto max-w-6xl mt-2">
+                    {!hasReachedEnd && data.slice(0, 5).map((pokemon: IPokemon) => (
+                        <PokedexEntry
+                            key={pokemon.pokedex_id}
+                            id={pokemon.pokedex_id}
+                            name={pokemon.name.fr}
+                            sprite={pokemon.sprites.regular}
+                            listTypes={pokemon.types}
+                        />
+                    ))}
+                </ol>
+            </main>
+        </div>
+    );
 }
