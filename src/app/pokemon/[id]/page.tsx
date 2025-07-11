31f6b1d871451e37621a9f43a9312b0fb2760243
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-07-11 08:35:04 +0200

    wip - Pokemon's details

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -1,5 +1,7 @@
-import { fetchPokemon } from "@/app/_api/tyradex";
+import Image from "next/image";
 
+import type { IPokemon, IPokemonType } from "@/app/_types/Pokemon";
+import { fetchPokemon } from "@/app/_api/tyradex";
 import { cleanString, typesAnimatedBorderColor } from "@/app/_utils/index";
 
 export default async function BlogPostPage({
@@ -10,7 +12,7 @@ export default async function BlogPostPage({
     const { id } = await params
     const pkmn = await fetchPokemon(Number(id))
 
-    const listTypes = pkmn.types.map((item:{name:string}) => item.name)
+    const listTypes = pkmn.types.map((item: { name: string }) => item.name)
 
     return (
         <div className="max-w-6xl mx-auto ">
@@ -21,8 +23,38 @@ export default async function BlogPostPage({
                     }}
                     className="sticky-header relative px-4 bg-white top-0 pt-2 pb-3 border-black border-solid border-b">
 
-                    <h1 className="text-3xl font-bold">#{String(id).padStart(4, '0')} {pkmn.name.fr}</h1>
-                    <p className="text-sm -mt-1">{pkmn.category}</p>
+                    <div className="flex flex-row gap-3 relative">
+                        <div className="w-20">
+                            <Image
+                                className="@xs:max-w-20 group-[.selected]:scale-85 group-hocus:scale-85 transition-transform"
+                                src={pkmn.sprites.regular}
+                                alt={`sprite de ${pkmn.name.fr}`}
+                                width={175}
+                                height={38}
+                                priority
+                            />
+                        </div>
+                        <div className="grow">
+                            <h1 className="text-3xl font-bold">#{String(id).padStart(4, '0')} {pkmn.name.fr}</h1>
+                            <p className="text-sm -mt-1">{pkmn.category}</p>
+                            <div className="@container">
+                                <ul className="flex gap-2 mt-2 flex-col @[10rem]:flex-row">
+                                    {pkmn.types.map(({name, image}: IPokemonType) => (
+                                        <li
+                                            className="py-0.5 px-2 rounded-md gap-1 flex items-center type-name w-fit"
+                                            aria-label="Type ${idx + 1} ${type.name}"
+                                            style={{
+                                                backgroundColor: `var(--type-${cleanString(name)})`
+                                            }}
+                                        >
+                                            <img className="h-5" src={image} alt={`icône type ${name}`} />
+                                            {name}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </header>
         </div>
