68c84556da29f324019c69c2c1958ba263a1db27
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-07-12 17:07:49 +0200

    Put in cache fetchPokemonForGeneration api function

--- src/app/_api/tyradex.ts
+++ src/app/_api/tyradex.ts
@@ -1,22 +1,27 @@
+import { cache } from 'react';
+
 import type { IPokemon, IPokemonType, IPokemonError } from "@/app/_types/Pokemon";
 
 const serverErrorStartNumber = 400;
 
-export const fetchPokemonForGeneration = async (generation = 1) => {
-    let hasReachedEnd = false;
+export const fetchPokemonForGeneration = cache(async (generation: number = 1) => {
+    let hasFoundGeneration = true;
     try {
-        const req: { status: number, json: () => Promise<IPokemon[]> } = await fetch(`https://tyradex.vercel.app/api/v1/gen/${generation}`);
+        const req: { status: number, json: () => Promise<IPokemon[]|IPokemonError> } = await fetch(`https://tyradex.vercel.app/api/v1/gen/${generation}`);
 
-        if (req.status >= serverErrorStartNumber) {
-            hasReachedEnd = true;
+        const res = await req.json();
+        const error = res as IPokemonError
+
+        if (error.status >= serverErrorStartNumber) {
+            hasFoundGeneration = false;
             throw new Error("");
         }
 
-        return { hasReachedEnd, data: await req.json() };
+        return { hasFoundGeneration, data: res };
     } catch (error) {
-        return { hasReachedEnd, data: [] };
+        return { hasFoundGeneration, data: [] };
     }
-}
+})
 
 export const fetchPokemon = async (pkmnId: number, region: string | null = null) => {
     try {
