f525fe8d48e24f461713ffe5519729a1b53bf166
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-07-12 14:11:13 +0200

    Display current generation in the header and in the previous/next links

--- src/app/_components/Pokedex.tsx
+++ src/app/_components/Pokedex.tsx
@@ -1,11 +1,27 @@
 "use client";
 
+import Link from 'next/link';
+
 import type { IPokemon } from "@/app/_types/Pokemon";
 import PokedexEntry from "@/app/_components/PokedexEntry";
 
 import { useLayout } from '@/app/_contexts/LayoutContext';
 
-export default ({ data }: { data: IPokemon[] }) => {
+import pkdexStyle from "./pokedex.module.css";
+
+const MAX_GENERATION = 9;
+
+const DexLink = ({ generation }: { generation: number; }) => {
+    return (
+        <li className="border-2 border-black rounded-md  bg-gray-200  hocus:text-white @container/pokemon overflow-hidden">
+            <Link data-generation={generation} href={`?id=${generation}`} className={`block hocus:bg-gray-500 p-2 w-full h-full ${pkdexStyle["pkdex-link"]} @sm:h-16`}>
+                Aller à la <span className='font-bold text-lg text-wrap'>GÉNÉRATION #{generation}</span>
+            </Link>
+        </li>
+    )
+}
+
+export default ({ data }: { data: IPokemon[]; }) => {
     const { layout } = useLayout();
     const listClasses = [
         ...(layout === "grid" ? ["grid-cols-3", "md:grid-cols-5", "lg:grid-cols-6"] : []),
@@ -16,8 +32,14 @@ export default ({ data }: { data: IPokemon[] }) => {
         return null;
     }
 
+    const currentGeneration = data[0].generation;
+
     return (
         <ol className={`pokedex grid gap-4 mb-4 mx-auto max-w-6xl mt-2 ${listClasses.join(" ")}`}>
+            {currentGeneration > 1 ? (
+                <DexLink generation={currentGeneration - 1} />
+            ) : null}
+
             {data.map((pokemon: IPokemon) => (
                 <PokedexEntry
                     key={pokemon.pokedex_id}
@@ -27,6 +49,9 @@ export default ({ data }: { data: IPokemon[] }) => {
                     listTypes={pokemon.types}
                 />
             ))}
+            {currentGeneration < MAX_GENERATION ? (
+                <DexLink generation={currentGeneration + 1} />
+            ) : null}
         </ol>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { fetchPokemonForGeneration } from "@/app/_api/tyradex";
 import Pokedex from "@/app/_components/Pokedex";
 import LayoutSwitch from "@/app/_components/LayoutSwitch";
 import { LayoutProvider } from '@/app/_contexts/LayoutContext';
+import { IPokemon } from "./_types/Pokemon";
 
 export default async function Home({
     searchParams,
@@ -10,15 +11,27 @@ export default async function Home({
     searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
     const { id = 1 } = await searchParams;
-    const { data, hasReachedEnd } = await fetchPokemonForGeneration(id as number);
+    const { data, hasFoundGeneration } = await fetchPokemonForGeneration(id as number);
 
     return (
         <LayoutProvider>
-            <header className="py-2 px-4 bg-slate-900 text-white sticky left-0 right-0 top-0 z-50 flex justify-end">
+            <header className="py-2 px-4 bg-slate-900 text-white sticky left-0 right-0 top-0 z-50 flex justify-between">
+                <div>
+                    <h2 className="text-2xl">
+                        Génération #{id}
+                    </h2>
+                    <p className="py-0.5 px-2 rounded-md bg-slate-600 text-white inline-flex">
+                        {hasFoundGeneration && <span>{String((data as IPokemon[])[0].pokedex_id).padStart(4, '0')} → {String(((data as IPokemon[]).at(-1) as IPokemon).pokedex_id).padStart(4, '0')}</span>}
+                    </p>
+                </div>
+
                 <LayoutSwitch />
             </header>
             <main className="px-4">
-                {!hasReachedEnd && <Pokedex data={data} />}
+                {hasFoundGeneration && (<Pokedex data={(data as IPokemon[])} />)}
+                {!hasFoundGeneration ? (
+                    <p>Impossible d'afficher cette génération, car elle n'existe pas.</p>
+                ) : null}
             </main>
         </LayoutProvider>
     );
