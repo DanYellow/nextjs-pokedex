08046b7346ae1683a0ba807e51960cdb9dc6e066
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-07-12 19:48:00 +0200

    Highlight Pokemon when hash is in url

--- src/app/_components/PokedexEntry.tsx
+++ src/app/_components/PokedexEntry.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 
-import { cleanString, typesAnimatedBorderColor } from "@/app/_utils/index";
+import { cleanString, typesAnimatedBorderColor, NB_NUMBER_INTEGERS_PKMN_ID } from "@/app/_utils/index";
 
 const TypeMarquee = ({ listTypes }: {listTypes: string[]}) => {
     return listTypes.map((type:string, typeIdx:number) => {
@@ -29,18 +29,20 @@ export default ({ id, name, sprite, listTypes: _listTypes }: { id: number, name:
     const listBorderClasses = typesAnimatedBorderColor[`${cleanString(listTypes[0])}_${cleanString(listTypes?.[1] || listTypes[0])}`]
 
     return (
-        <li className="@container/pokemon" id={`pkmn-${id}`}>
+        <li className="@container/pokemon" >
             <a href={`/pokemon/${id}`}
+                id={`pkmn-${id}`}
                 className={`
                     pokemon ripple-effect duration-150
                     border-type-animated
                     hocus:scale-105 @xs:hocus:scale-100
                     hocus:relative ease-out rounded-md py-2 group block border-solid
                     border-transparent border-2 outline-offset-2 transition-transform ${listBorderClasses}
+                    ${window.location.hash === `#pkmn-${id}` ? "bg-slate-200" : "" }
                 `}
             >
                 <div className="flex @xs:flex-row flex-col gap-3 items-center relative inert:opacity-50">
-                    <div className="absolute overflow-hidden inset-0 opacity-0 transition-opacity group-[.selected]:opacity-100 group-hocus:opacity-100 @xs:hidden h-fit" data-marquee>
+                    <div className="absolute overflow-hidden inset-0 opacity-0 transition-opacity group-[.selected]:opacity-100 group-hocus:opacity-100 @xs:hidden h-fit">
                         <TypeMarquee listTypes={listTypes} />
                     </div>
                     <Image
@@ -52,7 +54,7 @@ export default ({ id, name, sprite, listTypes: _listTypes }: { id: number, name:
                         loading="lazy"
                     />
                     <p className="group-hocus:pkmn-name group-[.selected]:pkmn-name @xs:text-left text-center whitespace-pre w-full">
-                        #{String(id).padStart(4, '0')}<br/>{name}
+                        #{String(id).padStart(NB_NUMBER_INTEGERS_PKMN_ID, '0')}<br/>{name}
                     </p>
                 </div>
             </a>
