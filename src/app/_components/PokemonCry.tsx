a765fc12b9343435475b053575dc86edb3701a66
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-07-28 16:14:56 +0200

    Add rollover effect to play button

--- src/app/_components/PokemonCry.tsx
+++ src/app/_components/PokemonCry.tsx
@@ -6,7 +6,7 @@ import { useCallback, useRef } from 'react';
 const PokemonCry = ({ color, link }: { color: string; link: URL }) => {
     const hexaColor = window.getComputedStyle(document.body).getPropertyValue(color);
     const hexaLightColor = `lch(from ${hexaColor} calc(l + 35) c h)`;
-    const hexaDarkColor = `lch(from ${hexaColor} calc(l - 35) c h)`;
+    const hexaDarkColor = `lch(from ${hexaColor} calc(l - 55) c h)`;
 
     const playerRef = useRef<HTMLDivElement>(null);
     const { wavesurfer, isPlaying } = useWavesurfer({
@@ -19,11 +19,11 @@ const PokemonCry = ({ color, link }: { color: string; link: URL }) => {
 
     const onPlayPause = useCallback(() => {
         wavesurfer && wavesurfer.playPause()
-    }, [wavesurfer]);
+    }, [wavesurfer])
 
     return (
         <div className="flex flex-row gap-4 items-center my-4 rounded-4xl px-5 py-2" style={{ backgroundColor: hexaDarkColor }}>
-            <button className={`rounded-full p-5`} style={{ backgroundColor: hexaColor, color: hexaLightColor }} onClick={onPlayPause} type="button">
+            <button className="rounded-full p-5 hocus:!bg-[color:var(--darken-bg-color)]" style={{ backgroundColor: hexaColor, color: hexaLightColor }} onClick={onPlayPause} type="button">
                 {isPlaying ? (
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                         <path fillRule="evenodd" d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z" clipRule="evenodd" />
