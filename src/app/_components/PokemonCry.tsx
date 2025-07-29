5a8a07fba5c1580ba665e26a21535647a2a72376
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-07-29 09:00:24 +0200

    Silent error message on window object undefined

--- src/app/_components/PokemonCry.tsx
+++ src/app/_components/PokemonCry.tsx
@@ -1,18 +1,24 @@
 "use client";
 
 import { useWavesurfer } from '@wavesurfer/react';
-import { useCallback, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 const PokemonCry = ({ color, link }: { color: string; link: URL }) => {
-    const hexaColor = window.getComputedStyle(document.body).getPropertyValue(color);
-    const hexaLightColor = `lch(from ${hexaColor} calc(l + 35) c h)`;
-    const hexaDarkColor = `lch(from ${hexaColor} calc(l - 55) c h)`;
+    const [baseColor, setBaseColor] = useState<string | null>(null);
+
+    useEffect(() => {
+        const hexaColor = window.getComputedStyle(document.body).getPropertyValue(color);
+        setBaseColor(hexaColor);
+    }, [])
 
     const playerRef = useRef<HTMLDivElement>(null);
+    const hexaLightColor = `lch(from ${baseColor} calc(l + 35) c h)`;
+    const hexaDarkColor = `lch(from ${baseColor} calc(l - 55) c h)`;
+
     const { wavesurfer, isPlaying } = useWavesurfer({
         container: playerRef,
         height: 75,
-        waveColor: hexaColor,
+        waveColor: baseColor || "transparent",
         progressColor: hexaLightColor,
         url: link.toString(),
     });
@@ -21,9 +27,13 @@ const PokemonCry = ({ color, link }: { color: string; link: URL }) => {
         wavesurfer && wavesurfer.playPause()
     }, [wavesurfer])
 
+    if (!baseColor) {
+        return null;
+    }
+
     return (
         <div className="flex flex-row gap-4 items-center my-4 rounded-4xl px-5 py-2" style={{ backgroundColor: hexaDarkColor }}>
-            <button className="rounded-full p-5 hocus:!bg-[color:var(--darken-bg-color)]" style={{ backgroundColor: hexaColor, color: hexaLightColor }} onClick={onPlayPause} type="button">
+            <button className="rounded-full p-5 hocus:!bg-[color:var(--darken-bg-color)]" style={{ backgroundColor: baseColor, color: hexaLightColor }} onClick={onPlayPause} type="button">
                 {isPlaying ? (
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                         <path fillRule="evenodd" d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z" clipRule="evenodd" />
