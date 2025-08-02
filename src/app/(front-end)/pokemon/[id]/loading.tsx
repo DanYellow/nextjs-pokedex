d23d2bfd6d7f07020d95c787b8fe1e469d0ca7bc
Danielo Jean-Louis (DanYellow) <jeanlouis.danielo@sfr.fr>
2025-08-02 09:30:02 +0200

    Improve loading page

--- src/app/(front-end)/pokemon/[id]/loading.tsx
+++ src/app/(front-end)/pokemon/[id]/loading.tsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
 
-async function Page() {
+async function Loading() {
     return (
-        <div>
+        <div className="flex flex-col items-center py-3 gap-10">
             <Image
                 src="/images/pikachu-loading.gif"
-                width={439}
-                height={321}
+                width={220}
+                height={160}
                 alt="Chargement"
-                className="block mx-auto scale-40"
                 unoptimized
             />
+            <p className="rounded-2xl bg-[#f0e208] border-[#bf2819] border border-solid px-2 py-0.5">Chargement</p>
         </div>
     )
 }
 
-export default Page;
+export default Loading;
