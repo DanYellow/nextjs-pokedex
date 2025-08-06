f79d89df9838d1f08272de56d70e5e7a81784d82
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-08-06 22:31:35 +0200

    Test cloneElement to pass props

--- src/app/(front-end)/pokemon/[id]/region/[name]/modal-wrapper.tsx
+++ src/app/(front-end)/pokemon/[id]/region/[name]/modal-wrapper.tsx
@@ -1,9 +1,17 @@
 "use client";
 
 import ClientOnlyPortal from "@/app/_components/ClientOnlyPortal";
-import { useRef } from "react";
+import { Children, cloneElement, isValidElement, ReactElement, useRef } from "react";
 
+
+interface InjectedProps {
+    handleClose?: () => void;
+}
 const Modal = ({ children }: { children: React.ReactNode }) => {
+    if (children && isValidElement(children) && Children.toArray(children).length !== 1) {
+        throw new Error("Slot must have exactly one child element");
+    }
+
     const dialogRef = useRef<HTMLDialogElement>(null);
 
     const onPortalReady = () => {
@@ -12,13 +20,28 @@ const Modal = ({ children }: { children: React.ReactNode }) => {
         }
     };
 
+    const onClose = () => {
+        if (dialogRef.current?.open) {
+            dialogRef.current?.close();
+        }
+    }
+
     return (
         <ClientOnlyPortal selector="#modal-container" onReady={onPortalReady}>
             <dialog
                 ref={dialogRef}
                 className={`sm:max-w-4xl w-full mx-auto my-auto px-6 pb-2 rounded-4xl backdrop:bg-slate-400/50 overscroll-y-contain bg-gray-50 border-solid border-2 border-(color:--modal-border-color)`}
             >
-                {children}
+                {Children.map(children, (child) => {
+                    if (isValidElement(child)) {
+
+                        return cloneElement<InjectedProps>(child as ReactElement, {
+                            handleClose: onClose,
+                        });
+                    }
+
+                    return child;
+                })}
             </dialog>
         </ClientOnlyPortal>
     );
