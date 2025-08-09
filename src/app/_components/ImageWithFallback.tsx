f387315bcd52b5fcf665e181d648b1ba7d2cfc8a
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-08-09 16:42:49 +0200

    Rename fallback component

---
+++ src/app/_components/ImageWithFallback.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+const ImageWithFallback = ({ src, alt, fallbackSrc = "/images/missing-cover.png", ...rest }: { src: string, alt: string, fallbackSrc?: string }) => {
+    const [imageHasError, setImageHasError] = useState(false);
+
+    return (
+        <Image
+            src={imageHasError ? fallbackSrc : src}
+            alt={alt}
+            onError={() => setImageHasError(true)}
+            {...rest}
+        />
+    );
+};
+
+export default ImageWithFallback;
